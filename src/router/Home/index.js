8033c7c94d332b957ceb65d35c2011d4954b7cea
asep jumadi <asepjumadi27@gmail.com>
2020-01-05 19:58:47 +0700

    update heading->change h2 of classname "line-progress" to h3 change heading h1 to h3 in below

--- src/router/Home/index.js
+++ src/router/Home/index.js
@@ -18,15 +18,15 @@ class Home extends Component {
       <div  className="row ">
         <div  className="col-7">
           <h4 className="line-drawing-demo" style={{color:'white',fontWeight: 'bold',paddingTop:80}}> Hi,I'am Asep</h4>
-            <h2 className="line-progress" style={{color:'white',fontWeight: 'bold',paddingTop:20,paddingLeft:5}}> WEB DEVELOPER </h2>
-          <h2 className="line-progress2" style={{color:'white',fontWeight: 'bold',paddingTop:20,paddingLeft:5}}>MOBILE DEVELOPER</h2>
+            <h3 className="line-progress" style={{color:'white',fontWeight: 'bold',paddingTop:20,paddingLeft:5}}> WEB DEVELOPER </h3>
+          <h3 className="line-progress2" style={{color:'white',fontWeight: 'bold',paddingTop:20,paddingLeft:5}}>MOBILE DEVELOPER</h3>
         </div>
         <div className="home_aspect_ratio col-4">
           <img src={require('../../assets/image/asep.png')} className="p-image--shadowed p_image--aspect" />
         </div>
       </div>
       <ul class="p-inline-images">
-      <h1 className="line-progress2" style={{color:'white',fontWeight: 'bold',marginTop:100}}>CI/CD Tools</h1>
+      <h3 className="line-progress2" style={{color:'white',fontWeight: 'bold',marginTop:100}}>CI/CD Tools</h3>
         <li class="p-inline-images__item">
           <img class="p-inline-images__logo" src={require('../../assets/image/heroku.png')} alt="Placeholder image"/>
           <h3 style={{color:'white'}}>HeroKu</h3>
